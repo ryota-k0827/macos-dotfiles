8e097bb4e5ecc067a2267dfc5fa39dfedee1492a
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2022-10-13 16:54:41 +0900

    fix system preferences

--- .bin/defaults.sh
+++ .bin/defaults.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 if [ "$(uname)" != "Darwin" ] ; then
 	echo "Not macOS!"
@@ -11,9 +11,30 @@ fi
 #
 # ====================
 
+# Show files with all extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Always display scrollbars
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+
+# Key repeat speed
+defaults write NSGlobalDomain KeyRepeat -int 2
 
 # Disable auto-capitalization
 defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+# Timing to start key repeat
+defaults write NSGlobalDomain InitialKeyRepeat -int 25
+
+# ====================
+#
+# DesktopServices
+#
+# ====================
+
+# Do not create .DS_Store files on USB or network storage
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 # ====================
 #
@@ -21,9 +42,11 @@ defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 #
 # ====================
 
+# Disable waiting time before the Dock is displayed
+defaults write com.apple.dock autohide-delay -float 0
 
 # Disable animation at application launch
-defaults write com.apple.dock launchanim -bool false
+# defaults write com.apple.dock launchanim -bool false
 
 # ====================
 #
@@ -33,19 +56,17 @@ defaults write com.apple.dock launchanim -bool false
 
 
 # Disable animation
-defaults write com.apple.finder DisableAllAnimations -bool true
+# defaults write com.apple.finder DisableAllAnimations -bool true
 
 # Show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
-
-# Show files with all extensions
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Display the status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
 # Display the path bar
 defaults write com.apple.finder ShowPathbar -bool true
+
 
 # ====================
 #
@@ -57,8 +78,16 @@ defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.menuextra.clock DateFormat -string 'EEE d MMM HH:mm'
 
 # Display battery level in the menu bar
-defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+# defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 
+# ====================
+#
+# Terminal
+#
+# ====================
+
+# Use UTF-8 only
+defaults write com.apple.terminal StringEncodings -array 4
 
 for app in "Dock" \
 	"Finder" \
--- README.md
+++ README.md
@@ -1,20 +1,12 @@
 # dotfiles
 
-&emsp;
 ## Overview
-
 This repository contains scripts to set macOS.
 
-&emsp;
-
 ## Required
-
 - macOS - v12.6
 
-&emsp;
-
 ## Install
-
 Download installation materials.
 
 ```shell
@@ -27,34 +19,18 @@ Set macOS.
 $ cd ~/dotfiles && make
 ```
 
-Set iterm2.
-
-
-```
-1. Press "command + ," on iterm2.
-2. Click "General" tab.
-3. Click "Preferences" tab.
-4. Turn on "Load preference from a custom folder or URL."
-5. Enter "~/dotfiles/iterm/com.googlecode.iterm2.plist".
-6. Execute "sudo killall cfprefsd".
-```
-
 Set visual studio code.
 
-```
 1. Press "shift + command + P" on visual studio code.
 2. Search and Click "Command: Install 'code' command in PATH command".
 3. Restart visual studio code.
 4. Execute "cd dotfiles && ./vscode/sync.sh".
 5. If you want to output the current extensions, execute "code --list-extensions > ~/dotfiles/vscode/extensions".
-```
 
 Set google chrome.
 
-```
 1. Access each URL in "~/dotfiles/chrome/extensions" with Google Chrome.
 2. Click "Add Chrome" button.
-```
 
 &emsp;
 
