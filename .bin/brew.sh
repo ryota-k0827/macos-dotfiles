173de9271bb6f46ca52649f8ed0d56fe35e12d55
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2022-11-23 00:32:04 +0900

    Fix brew scripts adn makefile

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -6,25 +6,28 @@ if [ "$(uname)" != "Darwin" ] ; then
 	exit 1
 fi
 
-echo "Open the App Store."
-sleep 1; echo "Please login with your Apple ID"
-sleep 3; open -a App\ Store
+function open_app_store {
+  echo "Please login with your Apple ID"
+  sleep 1; echo "Open the App Store."
+  sleep 1; open -a App\ Store
+}
 
-function ask_yes_no {
+function login_check {
   while true; do
-    echo -n "$* [Y/*]: "
+    echo -n "$* [Y/n]: (default: n) "
     read ANS
     case $ANS in
       [Yy]*)
         return 0
         ;;
       *)
-        echo "Please login with your Apple ID"
+        open_app_store
         ;;
     esac
   done
 }
 
-if ask_yes_no "Did you login?"; then
+open_app_store
+if login_check "Did you login?"; then
   brew bundle --global
 fi
--- .bin/setup.sh
+++ .bin/setup.sh
@@ -16,4 +16,4 @@ ssh-keygen -N '' -f ${SSH_KEY_PATH}/id_rsa
 pbcopy < ${SSH_KEY_PATH}/id_rsa.pub
 echo "SSH key copied to clipboard. Paste it into GitHub."
 sleep 1; echo "Open the GitHub settings page:"
-sleep 3; open https://github.com/settings/ssh/new
+sleep 1; open https://github.com/settings/ssh/new
