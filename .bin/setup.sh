8722198d7d69559a2a6456250c6f05aaf59628df
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2022-11-22 23:36:49 +0900

    Add ricty font setup

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -1,9 +1,16 @@
 #!/bin/zsh
 
 # Volta Setup
+echo -e "\033[0;34m- Volta Setup...\033[0m"
 volta setup
 
-# SSH Key Setup
+# Ricty Font Setup
+echo -e "\033[0;34m- Ricty Font Setup...\033[0m"
+cp -f /opt/homebrew/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+fc-cache -vf
+
+# GitHub SSH Setup
+echo -e "\033[0;34m- GitHub SSH Setup...\033[0m"
 SSH_KEY_PATH="$HOME/.ssh"
 ssh-keygen -N '' -f ${SSH_KEY_PATH}/id_rsa
 pbcopy < ${SSH_KEY_PATH}/id_rsa.pub
