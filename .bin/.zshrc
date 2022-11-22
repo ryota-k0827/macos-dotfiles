9a69dba5b5ca3669737dc92d242ffdeeff2bc56b
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2022-11-23 01:52:01 +0900

    Fix other_apps.sh

--- .bin/.zshrc
+++ .bin/.zshrc
@@ -9,6 +9,7 @@ alias ....='cd ../../..'
 alias relogin='exec $SHELL -l'
 
 alias rmtrash='rm -rf ${HOME}/.Trash/*'
+alias rmdownloads='rm -rf ${HOME}/Downloads/*'
 alias rmnm='rm -rf ./node_modules'
 
 alias delds='find . -name ".DS_Store" -type f -ls -delete'
--- .bin/other_apps.sh
+++ .bin/other_apps.sh
@@ -15,24 +15,24 @@ function ask_yes_no {
   done
 }
 
-function app_download_web {
+function app_dl_web {
   if ask_yes_no "Do you want to download $1?"; then
     sleep 1; echo "Download $1"
     sleep 1; open $2
   fi
 }
 
-function app_download_curl {
-  DL_DIR="$HOME/Downloads"
+function app_dl_curl {
+  DL_PATH="$HOME/Downloads/$(basename $2)"
   if ask_yes_no "Do you want to download $1?"; then
     sleep 1; echo "Download $1"
-    sleep 1; curl $2 --output "$DL_DIR/$3"
-    open "$DL_DIR/$3"
+    sleep 1; curl $2 --output $DL_PATH
+    open $DL_PATH
   fi
 }
 
 # DisplayLink Manager
-app_download_web "DisplayLink Manager" https://www.synaptics.com/products/displaylink-graphics/downloads/macos
+app_dl_web "DisplayLink Manager" https://www.synaptics.com/products/displaylink-graphics/downloads/macos
 
 # Logi Options+
-app_download_curl "Logi Options+" https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip logioptionsplus_installer.zip
+app_dl_curl "Logi Options+" https://download01.logi.com/web/ftp/pub/techsupport/optionsplus/logioptionsplus_installer.zip
