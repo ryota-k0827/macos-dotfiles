2b687f1cdf71f15aad3327f038f47b29fe357db3
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2023-02-11 14:35:40 +0900

    Add volta toolchain install commands

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -3,6 +3,8 @@
 # Volta Setup
 echo -e "\033[0;34m- Volta Setup...\033[0m"
 volta setup
+volta install node@lts
+volta install yarn@1.22.19
 
 # Ricty Font Setup
 echo -e "\033[0;34m- Ricty Font Setup...\033[0m"
