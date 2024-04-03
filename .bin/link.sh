405d3375afaf2c9296ef0a7b388ae75de1b08551
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2024-04-04 00:39:01 +0900

    brewfileのlaunchdによる自動作成スクリプトを作成

--- .bin/link.sh
+++ .bin/link.sh
@@ -1,13 +1,24 @@
 #!/bin/zsh
 
+# This script creates symbolic links in the home directory for all dotfiles (except .git, .github, and .DS_Store) in the script's directory, and then sources the .zprofile to apply the changes.
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 for dotfile in "${SCRIPT_DIR}"/.??* ; do
-    [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
-    [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
-    [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
+  [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
+  [[ "$dotfile" == "${SCRIPT_DIR}/.github" ]] && continue
+  [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
 
-    ln -fnsv "$dotfile" "$HOME"
+  ln -fnsv "$dotfile" "$HOME"
 done
 
 source ~/.zprofile
+
+
+# This script creates symbolic links in the user's Library/LaunchAgents directory for all files starting with 'com.' in the script's Library/LaunchAgents directory, and then loads each Launch Agent script using 'launchctl load'.
+for file in "${SCRIPT_DIR}/Library/LaunchAgents"/com.* ; do
+  ln -fnsv "$file" "$HOME/Library/LaunchAgents"
+
+  filename=$(basename -- "$file")
+  launchctl load "$HOME/Library/LaunchAgents/$filename"
+done
+
