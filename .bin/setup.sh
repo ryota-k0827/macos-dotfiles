7e8fa3269a98485d9e0aec1616baabd4037d0a3f
ryota-k0827 <50436249+ryota-k0827@users.noreply.github.com>
2024-04-07 16:43:01 +0900

    UDEV Gothicフォントのセットアップを追加

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -13,7 +13,13 @@ git clone https://github.com/romkatv/powerlevel10k-media.git
 \cp -f powerlevel10k-media/MesloLGS\ NF\ *.ttf ~/Library/Fonts/
 rm -rf powerlevel10k-media
 
-fc-cache -vf
+## UDEV Gothic
+echo -e "\033[0;34m- UDEV Gothic Font Setup...\033[0m"
+UDEV_GOTHIC_VERSION=1.3.1
+curl -L -O https://github.com/yuru7/udev-gothic/releases/download/v${UDEV_GOTHIC_VERSION}/UDEVGothic_v${UDEV_GOTHIC_VERSION}.zip
+unzip UDEVGothic_v${UDEV_GOTHIC_VERSION}.zip
+\cp -f UDEVGothic_v${UDEV_GOTHIC_VERSION}/*.ttf ~/Library/Fonts/
+rm -rf UDEVGothic_v${UDEV_GOTHIC_VERSION}.zip UDEVGothic_v${UDEV_GOTHIC_VERSION}
 
 # Chromium Setup
 echo -e "\033[0;34m- Chromium Setup...\033[0m"
