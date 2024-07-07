dbe738cc762b1311d521631c299dd6e4b0a82dec
kryota-dev <50436249+kryota-dev@users.noreply.github.com>
2024-07-07 12:16:12 +0900

    cssをjsに移行

--- vscode/custom.js
+++ vscode/custom.js
@@ -9,13 +9,28 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   waitForElement('.gettingStartedCategoriesContainer', (container) => {
+    container.style.gridTemplateAreas = `
+    ". uwu uwu uwu ."
+    ". header header header ."
+    ". left-column . right-column ."
+    ". footer footer footer ."
+  `;
+    container.style.gridTemplateRows = 'auto auto 1fr auto';
+
     const uwu = document.createElement('div');
     uwu.className = 'uwu';
+    uwu.style.gridArea = 'uwu';
+    uwu.style.display = 'flex';
+    uwu.style.justifyContent = 'center';
+    uwu.style.alignSelf = 'end';
+    uwu.style.paddingTop = '48px';
+    uwu.style.paddingBottom = '24px';
 
     const logo = document.createElement('img');
     logo.src = 'https://raw.githubusercontent.com/Aikoyori/ProgrammingVTuberLogos/main/VSCode/VSCode.png';
     logo.alt = 'uwu?';
     logo.className = 'logo';
+    logo.style.width = '400px';
 
     uwu.appendChild(logo);
 
