962d5f9e381f1fcc1df42a6b24433cc97a023a88
kryota-dev <50436249+kryota-dev@users.noreply.github.com>
2024-07-21 03:21:13 +0900

    Add scripts to change default browser to Brave and Chrome

---
+++ raycast/scripts/defaultbrowser-brave.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title defaultbrowser brave
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌏
+
+# Documentation:
+# @raycast.description Change default browser to Brave Browser.
+
+defaultbrowser browser
---
+++ raycast/scripts/defaultbrowser-chrome.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title defaultbrowser chrome
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌏
+
+# Documentation:
+# @raycast.description Change default browser to Google Chrome.
+
+defaultbrowser chrome
